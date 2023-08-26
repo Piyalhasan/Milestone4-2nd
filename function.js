4d04b0af9ec2c25a76bb4e4511f25391ff2e4741
piyalhasan <piyalhasan609@gmail.com>
2023-08-26 11:07:28 +0600

    function-object

---
+++ function.js
@@ -0,0 +1,19 @@
+// Function declaretion 
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+
+}
+
+
+
+// Call the function
+startFan();
+
+// 1.function key word likha
+// 2.function name dite hobe ,name er modde Letter ,digits,underscores,dollar thakte pare 
+// 3.name er por parentheses () dite hoy 
+// 4.parentheses er por curly bracket{} dite hoy
+// 5function k call korte hobe.tar por function run hobe 
+// একটা ফাংশন এর কোড একবার লিখলেই হয়,তার পর যত বার ইচ্ছা ততবার ব্যবহার করা যায়
